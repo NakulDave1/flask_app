c5f5870b4bb1f02964ca134ab0c7fd64bed179be
NakulDave1 <nakuldavect19@acropolis.in>
2022-08-07 00:20:53 +0530

    initial commit

---
+++ web_app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/user/<name>')
+def main_page(name,password='hpsystem'):
+    text=['<h1>',"This", 'is' ,'HP', 'System' ,'Data' ,'</h1>']
+    return render_template('index.html',name=name,password=password,text=text)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
